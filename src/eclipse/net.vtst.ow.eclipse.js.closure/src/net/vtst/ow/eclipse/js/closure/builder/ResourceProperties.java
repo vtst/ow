a462579b45e2e24e8a7fb5688eab5cb85f474b28
vtst <github.self@vtst.net>
2012-03-25 15:48:49 +0200

    Document.

--- src/eclipse/net.vtst.ow.eclipse.js.closure/src/net/vtst/ow/eclipse/js/closure/builder/ResourceProperties.java
+++ src/eclipse/net.vtst.ow.eclipse.js.closure/src/net/vtst/ow/eclipse/js/closure/builder/ResourceProperties.java
@@ -11,26 +11,46 @@ import org.eclipse.core.resources.IProject;
 import org.eclipse.core.runtime.CoreException;
 import org.eclipse.core.runtime.QualifiedName;
 
+/**
+ * The plugin stores references to its own objects in session properties of resources,
+ * in order to allow easy and quick retrieval.  This class provides abstract methods for
+ * accessing these properties in a type safe way.
+ * @author Vincent Simonet
+ */
 public class ResourceProperties {
   
   private static final QualifiedName JS_PROJECT = new QualifiedName(OwJsClosurePlugin.PLUGIN_ID, "JSProject");
   private static final QualifiedName JS_FILES = new QualifiedName(OwJsClosurePlugin.PLUGIN_ID, "Files");
   private static final QualifiedName JS_UNIT = new QualifiedName(OwJsClosurePlugin.PLUGIN_ID, "JSUnit");
   
+  /**
+   * Set the {@code JSProject} associated with a resource project.
+   */
   public static void setJSProject(IProject project, JSProject jsProject) throws CoreException {
     project.setSessionProperty(JS_PROJECT, jsProject);
   }
   
+  /**
+   * Get the {@code JSProject} associated with a resource project, or null if there is no.
+   */
   public static JSProject getJSProject(IProject project) throws CoreException {
     Object obj = project.getSessionProperty(JS_PROJECT);
     if (obj instanceof JSProject) return (JSProject) obj;
     return null;
   }
   
+  /**
+   * Set the collection of JavaScript files which are associated with a project. This is
+   * a subset of the project's files that the plugin's builder handles as JavaScript files.
+   */
   public static void setJavaScriptFiles(IProject project, Collection<IFile> files) throws CoreException {
     project.setSessionProperty(JS_FILES, files);
   }
   
+  /**
+   * Get the collection of JavaScript files which are associated with a project.  This
+   * might be {@code null} or empty.
+   */
   @SuppressWarnings("unchecked")
   public static Collection<IFile> getJavaScriptFiles(IProject project) throws CoreException {
     Object obj = project.getSessionProperty(JS_PROJECT);
@@ -38,16 +58,26 @@ public class ResourceProperties {
     return null;    
   }
   
+  /**
+   * Set the collection of JavaScript files which are associated with a project.
+   */
   public static void setJSUnit(IFile file, CompilableJSUnit unit) throws CoreException {
     file.setSessionProperty(JS_UNIT, unit);
   }
   
+  /**
+   * Get the {@code CompilableJSUnit} which is associated with a (JavaScript) file,
+   * or null if there is no.
+   */
   public static CompilableJSUnit getJSUnit(IFile file) throws CoreException {
     Object obj = file.getSessionProperty(JS_UNIT);
     if (obj instanceof CompilableJSUnit) return (CompilableJSUnit) obj;
     return null;
   }
 
+  /**
+   * Set the {@code CompilableJSUnit} which is associated with a (JavaScript) file.
+   */
   public static CompilableJSUnit getJSUnitOrNullIfCoreException(IFile file) {
     try {
       return getJSUnit(file);
