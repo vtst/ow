de63f079ec454410b7284f8f28b6da8c1b0d6e1f
vtst <github.self@vtst.net>
2012-04-14 16:11:30 +0200

    Add missing files

---
+++ src/closure/net.vtst.ow.closure.compiler/src/net/vtst/ow/closure/compiler/strip/StrippableJsAst.java
@@ -0,0 +1,32 @@
+package net.vtst.ow.closure.compiler.strip;
+
+import com.google.javascript.jscomp.AbstractCompiler;
+import com.google.javascript.jscomp.JsAst;
+import com.google.javascript.jscomp.NodeTraversal;
+import com.google.javascript.jscomp.SourceFile;
+import com.google.javascript.rhino.Node;
+
+public class StrippableJsAst extends JsAst {
+  private static final long serialVersionUID = 1L;
+  
+  private Node strippedRoot;
+
+  public StrippableJsAst(SourceFile sourceFile) {
+    super(sourceFile);
+  }
+  
+  public void clearAst() {
+    super.clearAst();
+    strippedRoot = null;
+  }
+
+  public Node getStrippedAstRoot(AbstractCompiler compiler) {
+    if (strippedRoot == null) {
+      strippedRoot = getAstRoot(compiler).cloneTree();
+      NodeTraversal traversal = new NodeTraversal(compiler, new StripNodeTraversalCallback());
+      traversal.traverse(strippedRoot);
+    }
+    return strippedRoot;
+  }
+  
+}
