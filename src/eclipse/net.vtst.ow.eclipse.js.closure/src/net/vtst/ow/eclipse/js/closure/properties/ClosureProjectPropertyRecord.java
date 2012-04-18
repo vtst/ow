c080f6a1413370225dcae700c80ae6304d588d3a
vtst <github.self@vtst.net>
2012-04-18 20:47:16 +0200

    Closure linter properties.

--- src/eclipse/net.vtst.ow.eclipse.js.closure/src/net/vtst/ow/eclipse/js/closure/properties/ClosureProjectPropertyRecord.java
+++ src/eclipse/net.vtst.ow.eclipse.js.closure/src/net/vtst/ow/eclipse/js/closure/properties/ClosureProjectPropertyRecord.java
@@ -5,6 +5,8 @@ import net.vtst.eclipse.easy.ui.properties.fields.BooleanField;
 import net.vtst.eclipse.easy.ui.properties.fields.EnumOptionsField;
 import net.vtst.eclipse.easy.ui.properties.fields.FileField;
 import net.vtst.eclipse.easy.ui.properties.fields.FileListField;
+import net.vtst.eclipse.easy.ui.properties.fields.FlagListField;
+import net.vtst.eclipse.easy.ui.properties.fields.StringField;
 
 import com.google.javascript.jscomp.CompilerOptions;
 import com.google.javascript.jscomp.CompilerOptions.LanguageMode;
@@ -42,6 +44,33 @@ public class ClosureProjectPropertyRecord extends Record {
   }
   
   public ChecksRecord checks = new ChecksRecord();
+  
+  // Linter
+  
+  /*
+   * TODO
+   *     - blank_lines_at_top_level: validatesnumber of blank lines between blocks at
+    top level.
+    - indentation: checks correct indentation of code.
+    - well_formed_author: validates the @author JsDoc tags.
+    - no_braces_around_inherit_doc: forbids braces around @inheritdoc JsDoc
+    tags.
+    - braces_around_type: enforces braces around types in JsDoc tags.
+    - optional_type_marker: checks correct use of optional marker = in param
+    types.
+
+   */
+  
+  public static class LinterRecord extends Record {
+    public StringField customJsdocTags = new StringField("");
+    public FlagListField lintErrorChecks = new FlagListField(new String[]{
+        "blank_lines_at_top_level", "indentation", "well_formed_author", "no_braces_around_inherit_doc", "braces_around_type", "optional_type_marker"});
+    public BooleanField strictClosureStyle = new BooleanField(false);
+    public BooleanField missingJsdoc = new BooleanField(true);
+    public StringField ignoreLintErrors = new StringField("");
+  }
+  
+  public LinterRecord linter = new LinterRecord();
 
   private static ClosureProjectPropertyRecord instance;
   public static ClosureProjectPropertyRecord getInstance() {
