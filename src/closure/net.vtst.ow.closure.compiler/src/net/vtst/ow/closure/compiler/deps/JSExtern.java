ef8e6fad247bc7f09f9d727c18086a156c8b90c3
vtst <github.self@vtst.net>
2012-04-19 15:20:33 +0100

    Commit changes.

--- src/closure/net.vtst.ow.closure.compiler/src/net/vtst/ow/closure/compiler/deps/JSExtern.java
+++ src/closure/net.vtst.ow.closure.compiler/src/net/vtst/ow/closure/compiler/deps/JSExtern.java
@@ -5,10 +5,15 @@ import java.io.File;
 import net.vtst.ow.closure.compiler.deps.JSLibrary.CacheMode;
 
 import com.google.javascript.jscomp.JSSourceFile;
+import com.google.javascript.jscomp.SourceFile;
 
 public class JSExtern extends AstFactory {
   private static final long serialVersionUID = 1L;
 
+  public JSExtern(SourceFile sourceFile) {
+    super(sourceFile);
+  }
+  
   public JSExtern(String name, JSUnitProvider.IProvider provider) {
     super(JSSourceFile.fromGenerator(name, provider));
   }
