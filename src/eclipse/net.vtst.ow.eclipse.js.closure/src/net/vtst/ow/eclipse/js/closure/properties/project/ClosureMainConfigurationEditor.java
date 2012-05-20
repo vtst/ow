e49910beb6b45845cafc62bcd2195cc70da39f29
vtst <github.self@vtst.net>
2012-05-20 22:20:05 +0200

    Add help message on project property page.

--- src/eclipse/net.vtst.ow.eclipse.js.closure/src/net/vtst/ow/eclipse/js/closure/properties/project/ClosureMainConfigurationEditor.java
+++ src/eclipse/net.vtst.ow.eclipse.js.closure/src/net/vtst/ow/eclipse/js/closure/properties/project/ClosureMainConfigurationEditor.java
@@ -1,7 +1,12 @@
 package net.vtst.ow.eclipse.js.closure.properties.project;
 
+import org.eclipse.swt.SWT;
+import org.eclipse.swt.layout.GridData;
+import org.eclipse.swt.widgets.Label;
+
 import net.vtst.eclipse.easy.ui.properties.editors.DefaultCompoundEditor;
 import net.vtst.eclipse.easy.ui.properties.editors.IEditorContainer;
+import net.vtst.eclipse.easy.ui.util.SWTFactory;
 
 public class ClosureMainConfigurationEditor extends DefaultCompoundEditor {
   
@@ -9,5 +14,10 @@ public class ClosureMainConfigurationEditor extends DefaultCompoundEditor {
   
   public ClosureMainConfigurationEditor(IEditorContainer container) {
     super(container, 1);
+    Label horizontalLine = new Label(getComposite(), SWT.SEPARATOR | SWT.HORIZONTAL);
+    SWTFactory.createVerticalSpacer(getComposite(), 1);
+    horizontalLine.setLayoutData(new GridData(GridData.FILL, GridData.FILL, true, false, 2, 1));
+    horizontalLine.setFont(getComposite().getFont());
+    SWTFactory.createWrapLabel(getComposite(), getMessage("help"), 1, 10);
   }
 }
