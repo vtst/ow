81e09f908bad772e480ce8509dd862ffc7112704
vtst <git@vtst.net>
2012-05-25 23:57:43 +0200

    Add missing file to index.

---
+++ src/eclipse/net.vtst.ow.eclipse.less/src/net/vtst/ow/eclipse/less/parser/LessValueConverterService.java
@@ -0,0 +1,31 @@
+package net.vtst.ow.eclipse.less.parser;
+
+import org.eclipse.xtext.conversion.IValueConverter;
+import org.eclipse.xtext.conversion.ValueConverter;
+import org.eclipse.xtext.conversion.ValueConverterException;
+import org.eclipse.xtext.conversion.impl.AbstractDeclarativeValueConverterService;
+import org.eclipse.xtext.nodemodel.INode;
+
+public class LessValueConverterService extends AbstractDeclarativeValueConverterService {
+
+  private IValueConverter<String> stringValueConverter = new IValueConverter<String>() {
+
+    @Override
+    public String toValue(String string, INode node)
+        throws ValueConverterException {
+      return string;
+    }
+
+    @Override
+    public String toString(String value) throws ValueConverterException {
+      return value;
+    }
+    
+  };
+  
+  @ValueConverter(rule = "STRING")
+  public IValueConverter<String> getSTRINGConverter() {
+    return stringValueConverter;
+  }
+
+}
