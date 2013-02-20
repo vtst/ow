695a9f3fa56e081906f3f601fd0dd7772c072f91
vtst <github.self@vtst.net>
2013-02-20 21:49:57 +0530

    Project nature + enabling property page.

---
+++ src/eclipse/net.vtst.ow.eclipse.less.ui/src/net/vtst/ow/eclipse/less/ui/nature/ToggleLessNatureAction.java
@@ -0,0 +1,12 @@
+package net.vtst.ow.eclipse.less.ui.nature;
+
+import net.vtst.eclipse.easyxtext.ui.nature.ToggleNatureAction;
+
+public class ToggleLessNatureAction extends ToggleNatureAction {
+
+  @Override
+  protected String getNatureId() {
+    return "net.vtst.ow.eclipse.less.nature";
+  }
+
+}
