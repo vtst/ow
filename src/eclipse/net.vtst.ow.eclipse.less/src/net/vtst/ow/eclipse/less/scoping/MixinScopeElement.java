e961b84b9f9aa561dab86e3d02f9c1487dae221a
vtst <github.self@vtst.net>
2013-05-12 17:10:52 +0200

    Add documentation.

--- src/eclipse/net.vtst.ow.eclipse.less/src/net/vtst/ow/eclipse/less/scoping/MixinScopeElement.java
+++ src/eclipse/net.vtst.ow.eclipse.less/src/net/vtst/ow/eclipse/less/scoping/MixinScopeElement.java
@@ -7,24 +7,50 @@ import org.eclipse.xtext.naming.QualifiedName;
 import org.eclipse.xtext.resource.EObjectDescription;
 import org.eclipse.xtext.resource.IEObjectDescription;
 
+/**
+ * An element of a mixin scope.  It can be converted into an object description.
+ * 
+ * @author Vincent Simonet
+ */
 public class MixinScopeElement {
   
   private String name;
   private ArrayList<EObject> objects;
   
+  /**
+   * Create a root element. 
+   */
   public MixinScopeElement() {
     this(null, new ArrayList<EObject>());
   }
   
+  /**
+   * Create a non-root element.
+   * @param name  The name of the element.
+   * @param objects  The list of hashOrClass that point to this element.
+   */
   private MixinScopeElement(String name, ArrayList<EObject> objects) {
     this.name = name;
     this.objects = objects;
   }
   
+  /**
+   * @return  The name of the element.
+   */
   public String getName() { return name; }
   
+  /**
+   * @param index
+   * @return The hashOrClass at the index-th position.
+   */
   public EObject getObject(int index) { return objects.get(index); }
   
+  /**
+   * Create a new element, with the same list of objects + one object.
+   * @param name
+   * @param pushedObject
+   * @return  The new element.
+   */
   public MixinScopeElement cloneAndExtends(String name, EObject pushedObject) {
     @SuppressWarnings("unchecked")
     ArrayList<EObject> newObjects = (ArrayList<EObject>) this.objects.clone();
@@ -32,7 +58,10 @@ public class MixinScopeElement {
     return new MixinScopeElement(name, newObjects);
   }
 
-  private QualifiedName asQualifiedName() {
+  /**
+   * @return The qualified name for the current element.
+   */
+  private QualifiedName getQualifiedName() {
     if (this.name.startsWith(".")) {
       // This is because of the special interpretation of '.' by xtext when doing linking.
       // TODO A better approach would be to de-activate the special handling of . by implementing
@@ -43,8 +72,13 @@ public class MixinScopeElement {
     }
   }
   
+  /**
+   * Converts the element into an object description.
+   * @param index
+   * @return
+   */
   public IEObjectDescription asEObjectDescription(int index) {
-    return EObjectDescription.create(asQualifiedName(), getObject(index));
+    return EObjectDescription.create(getQualifiedName(), getObject(index));
   }
   
 }
