f279b150e5c259b48425fb0d9489e9ff67fb014c
vtst <github.self@vtst.net>
2014-03-01 18:18:34 +0100

    LessImportingStatementFinder

--- src/eclipse/net.vtst.ow.eclipse.less/src/net/vtst/ow/eclipse/less/resource/LessResourceDescriptionStrategy.java
+++ src/eclipse/net.vtst.ow.eclipse.less/src/net/vtst/ow/eclipse/less/resource/LessResourceDescriptionStrategy.java
@@ -1,5 +1,6 @@
 package net.vtst.ow.eclipse.less.resource;
 
+import net.vtst.ow.eclipse.less.less.LessPackage;
 import net.vtst.ow.eclipse.less.less.StyleSheet;
 
 import org.eclipse.emf.common.util.URI;
@@ -9,6 +10,7 @@ import org.eclipse.xtext.resource.EObjectDescription;
 import org.eclipse.xtext.resource.IDefaultResourceDescriptionStrategy;
 import org.eclipse.xtext.resource.IEObjectDescription;
 import org.eclipse.xtext.resource.IReferenceDescription;
+import org.eclipse.xtext.resource.IResourceDescription;
 import org.eclipse.xtext.util.IAcceptor;
 
 /**
@@ -33,4 +35,16 @@ public class LessResourceDescriptionStrategy implements IDefaultResourceDescript
     return false;
   }
 
+  public static StyleSheet getStyleSheet(IResourceDescription desc) {
+    for (IEObjectDescription objectDesc : desc.getExportedObjectsByType(LessPackage.eINSTANCE.getStyleSheet())) {
+      if (LessResourceDescriptionStrategy.STYLESHEET_NAME.equals(objectDesc.getQualifiedName())) {
+        EObject obj = objectDesc.getEObjectOrProxy();
+        if (obj instanceof StyleSheet) {
+          return (StyleSheet) obj;
+        }
+      }
+    }
+    return null;
+  }
+  
 }
